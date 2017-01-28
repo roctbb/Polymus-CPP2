4c30244314ef46cf5e1129b94415b64ba7a9d4ba
roctbb <roctbb@gmail.com>
2017-01-28 21:48:31 +0300

    first commit

---
+++ Classroom/Lessons1-2/AccountSimple/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Account
+{
+private:
+    int id;
+    string name;
+    int balance;
+    int overdraft;
+public:
+    Account(int userId, string userName, int userBalance, int userOverdraft) {
+        id = userId;
+        name = userName;
+        overdraft = userOverdraft;
+        balance = userBalance;
+        string filename = to_string(userId)+".txt";
+        ifstream fin(filename);
+
+    }
+    ~Account() {
+    }
+    string GetName() {
+        return name;
+    }
+    int GetBalance() {
+        return balance;
+    }
+    bool ChangeBalance(int amount) {
+        if (balance+amount+overdraft>=0) {
+            balance+=amount;
+            return true;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    Account account(1, "Vasya Pupkin", 100, 200);
+    while(true)
+    {
+        int amount;
+        cin>>amount;
+        if (account.ChangeBalance(amount))
+        {
+            cout<<"OK, balance: "<<account.GetBalance()<<endl;
+        }
+        else
+        {
+            cout<<"ERROR, balance: "<<account.GetBalance()<<endl;
+        }
+    }
+
+}
